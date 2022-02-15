5c73f0700ebc212375a84708b87384ab5f9a8b6c
Papo00 <ye068@live.mdx.ac.uk>
2022-02-15 23:12:29 +0000

    Update z.redled_at specific_place_in_whitelines.py

--- examples/z.redled_at specific_place_in_whitelines.py
+++ examples/z.redled_at specific_place_in_whitelines.py
@@ -2,40 +2,40 @@ import opc
 import time
 import random
 
-leds = [(255,255,255)]*360 #red,green,blue so you get a while one
+leds = [(255,255,255)]*360 #white leds
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[59] = (255,0,0) #to make pixel 10 red
+leds[59] = (255,0,0) #pixel 59 red
 time.sleep(0.1) #delay
-client.put_pixels(leds)#to place it using put pixel method
+client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[59+60] = (0,255,0) #to make pixel 10 red
-time.sleep(0.1) #delay
-client.put_pixels(leds)#to place it using put pixel method
+leds[59+60] = (0,255,0) #pixel 119 green
+time.sleep(0.1) 
+client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[119+60] = (0,0,255) #to make pixel 10 red
-time.sleep(0.1) #delay
-client.put_pixels(leds)#to place it using put pixel method
+leds[119+60] = (0,0,255) #pixel 179 blue
+time.sleep(0.1) 
+client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[179+60] = (245,0,222) #to make pixel 10 red
-time.sleep(0.1) #delay
-client.put_pixels(leds)#to place it using put pixel method
+leds[179+60] = (245,0,222) #pixel 239 pink
+time.sleep(0.1) 
+client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[239+60] = (235,235,35) #to make pixel 10 red
-time.sleep(0.1) #delay
-client.put_pixels(leds)#to place it using put pixel method
+leds[239+60] = (235,235,35) #pixel 299 yellow
+time.sleep(0.1)
+client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[299+60] = (90,200,150) #to make pixel 10 red
-time.sleep(0.1) #delay
-client.put_pixels(leds)#to place it using put pixel method
+leds[299+60] = (90,200,150) #pixel 259 light green
+time.sleep(0.1) 
+client.put_pixels(leds)
 client.put_pixels(leds)
 
 
