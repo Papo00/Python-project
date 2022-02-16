ab4d85b8cdb8cac3307655cdecf7f94163f8e746
Papo00 <ye068@live.mdx.ac.uk>
2022-02-16 03:15:30 +0000

    Netherland_flag

---
+++ examples/Netherland_flag.py
@@ -0,0 +1,30 @@
+import opc
+import time
+from time import sleep
+
+leds=[(0,0,0)]*360
+client = opc.Client('localhost:7890')
+
+client.put_pixels(leds)
+client.put_pixels(leds)
+
+led = 0
+while led<60:
+    for rows in range(2): #first 2 rows left to right
+        leds[led+rows*60] = (255,0,0)
+    for rows in range (4,6): #reverse last 2 rows right to left
+        leds[59-led + rows*60] = (0,0,255)  
+    client.put_pixels(leds)
+    time.sleep(.1)
+    led = led + 1
+
+#from middle to right and left
+led = 30  #starting from the middle so we put 30
+while led>=0: 
+    for rows in range (2,4):     #allocating the middle row
+        leds[led + rows*60] = (255,255,255) # from middle to right
+        leds[59- led + rows*60] = (255,255,255)#from middle to left
+
+    client.put_pixels(leds)
+    time.sleep(.1)
+    led = led - 1
